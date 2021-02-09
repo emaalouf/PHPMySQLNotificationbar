57f725a88f9e7e05018f09fc121e16a198c57552
Elias Maalouf <egmaalouf@gmail.com>
2021-02-10 01:03:55 +0200

    First commit

---
+++ notification.php
@@ -0,0 +1,20 @@
+<?php SESSION_START(); 
+include ('Push.php');  
+$push = new Push(); 
+$array=array(); 
+$rows=array(); 
+$notifList = $push->listNotificationUser($_SESSION['username']); 
+$record = 0;
+foreach ($notifList as $key) {
+ $data['title'] = $key['title'];
+ $data['msg'] = $key['notif_msg'];
+ $rows[] = $data;
+ $nextime = date('Y-m-d H:i:s',strtotime(date('Y-m-d H:i:s'))+($key['notif_repeat']*60));
+ $push->updateNotification($key['id'],$nextime);
+ $record++;
+}
+$array['notif'] = $rows;
+$array['count'] = $record;
+$array['result'] = true;
+echo json_encode($array);
+?>
